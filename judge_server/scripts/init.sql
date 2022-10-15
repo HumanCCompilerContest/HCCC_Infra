316bf66a9eb8d44904e920142debe0be2360e30d
Alingof <Alignof@outlook.com>
2022-10-16 02:22:11 +0900

    [update] update sql to initialize database

--- judge_server/scripts/init.sql
+++ judge_server/scripts/init.sql
@@ -1,6 +1,40 @@
-CREATE TABLE tweets
-(
-    id        serial primary key,
-    message   text        not null,
-    posted_at timestamptz not null
+CREATE TYPE JudgeResult AS ENUM (
+    'AC',
+    'WA',
+    'AE',
+    'LE',
+    'TLE',
+    'SystemError'
+);
+
+CREATE TABLE players ( -- ユーザ
+	id serial primary key,
+	name text not null,
+	password text not null
+    -- session data?
+);
+
+CREATE TABLE probrems ( -- 問題
+	id serial primary key,
+	title text not null,
+	statement text not null,
+	code text not null,
+	input_desc text,
+	output_desc text,
+	score integer not null
+);
+
+CREATE TABLE submits ( -- submit
+	id serial primary key,
+	user_id serial not null,
+	time timestamptz not null,
+	asem text not null,
+	result JudgeResult not null
+);
+
+CREATE TABLE testcases ( -- テストケース
+	id serial primary key,
+	sepecial_cond serial,
+	input text not null,
+	output text not null
 );
