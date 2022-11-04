d8688de218363c43172bba6f93fda01ac26d3fa3
Alingof <Alignof@outlook.com>
2022-11-04 23:02:18 +0900

    [add] support for json format testcase

--- judge_system/src/run_test.rs
+++ judge_system/src/run_test.rs
@@ -1,8 +1,16 @@
 use std::time::Duration;
 use tokio::process::Command;
+use serde::Deserialize;
 use crate::{ExitCode, ExeOption};
 
 const TLE_SEC: u64 = 2;
+
+#[derive(Deserialize)]
+struct Testcase {
+    _id: i32,
+    input: String,
+    expect: String,
+}
 
 pub async fn just_exec() {
     // exec
@@ -28,25 +36,21 @@ pub async fn just_exec() {
 }
 
 pub async fn with_testcase(testcase_number: &str, exe_option: ExeOption) {
-    let testcases = std::fs::read_to_string(format!("./testcase/{}.case", testcase_number))
+    let testcase_str = std::fs::read_to_string(format!("./testcase/case{}.json", testcase_number))
         .unwrap_or_else(|_| {
             eprintln!("System Error");
             std::process::exit(ExitCode::SystemError as i32);
         });
-    let testcases = testcases
-        .lines()
-        .collect::<Vec<_>>();
+    let testcases: Vec<Testcase> = serde_json::from_str(&testcase_str).unwrap();
 
-    for case in testcases.chunks(2) {
-        let [input, expect]: [&str; 2] = case.try_into().expect("get error");
-
+    for case in testcases {
         // exec and test
         let output = tokio::time::timeout(
             Duration::from_secs(TLE_SEC as u64),
             Command::new("bash")
                 .kill_on_drop(true)
                 .arg("-c")
-                .arg(format!("echo {} | ./test_target", input))
+                .arg(format!("echo {} | ./test_target", case.input))
                 .output()
         )
         .await
@@ -62,7 +66,7 @@ pub async fn with_testcase(testcase_number: &str, exe_option: ExeOption) {
         match exe_option {
             ExeOption::ExitCode => {
                 let exit_status = output.status.code().unwrap();
-                let expect = expect.parse().unwrap();
+                let expect: i32 = case.expect.parse().unwrap();
                 if exit_status != expect {
                     eprintln!("output: {:?}", exit_status);
                     std::process::exit(ExitCode::WA as i32);
@@ -70,7 +74,7 @@ pub async fn with_testcase(testcase_number: &str, exe_option: ExeOption) {
             },
             ExeOption::Output => {
                 let stdout = String::from_utf8_lossy(&output.stdout);
-                if stdout != expect {
+                if stdout != case.expect {
                     eprintln!("output: {:?}", stdout);
                     std::process::exit(ExitCode::WA as i32);
                 }
