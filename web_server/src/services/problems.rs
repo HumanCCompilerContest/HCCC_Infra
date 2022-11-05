7b8c6f47938def2e68b6327168b5baf4270687c9
Alingof <Alignof@outlook.com>
2022-11-06 00:55:20 +0900

    [add] add services/problems.rs

---
+++ web_server/src/services/problems.rs
@@ -0,0 +1,17 @@
+use crate::entities::{Problem, AllProblems};
+use crate::repositories::Problems;
+
+pub async fn get_problem(repo: &impl Problems, problem_id: i32) -> Problem {
+   repo.find_problem(problem_id)
+       .await
+       .unwrap_or(Problem::error())
+}
+
+pub async fn get_all_problems(repo: &impl Problems) -> AllProblems {
+    AllProblems::new(
+        "ok".to_string(),
+        repo.all_problems().await,
+        None,
+    )
+}
+
