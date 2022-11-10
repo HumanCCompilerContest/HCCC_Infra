b13b41851c6ed82e1494c246886ab59e18d85425
Alingof <Alignof@outlook.com>
2022-11-10 18:40:09 +0900

    [update] implement ToSql to Rank and Ranking

--- web_server/src/entities/ranking.rs
+++ web_server/src/entities/ranking.rs
@@ -1,13 +1,37 @@
+use serde::Serialize;
+use postgres_types::ToSql;
+
+#[allow(non_snake_case)]
+#[derive(Serialize)]
+#[derive(Debug, ToSql)]
 pub struct Rank {
     rank: i32,
     userName: String,
     score: i32,
 }
 
+#[allow(non_snake_case)]
+#[derive(Serialize)]
+#[derive(Debug, ToSql)]
 pub struct Ranking {
     status: String,
     ranks: Vec<Rank>,
     errorMessage: Option<String>,
+}
+
+impl Rank {
+    pub fn new(user_name: String, score: i32) -> Self {
+        Rank {
+            rank: 0,
+            userName: user_name,
+            score,
+        }
+    }
+
+    pub fn set_rank(mut self, rank: usize) -> Self {
+        self.rank = rank as i32;
+        self
+    }
 }
 
 impl Ranking {
@@ -16,6 +40,14 @@ impl Ranking {
             status: "ok".to_string(),
             ranks,
             errorMessage: None,
+        }
+    }
+
+    pub fn error() -> Self {
+        Ranking {
+            status: "ng".to_string(),
+            ranks: Vec::new(),
+            errorMessage: Some("failed to create ranking".to_string()),
         }
     }
 }
