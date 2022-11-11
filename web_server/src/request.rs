e22bded0f74d897f22144238a59226ae86919d6d
Alingof <Alignof@outlook.com>
2022-11-11 21:21:12 +0900

    [update] modify UserContext implementation to return Json

--- web_server/src/request.rs
+++ web_server/src/request.rs
@@ -1,10 +1,12 @@
-use crate::constants::{database_url, AXUM_SESSION_COOKIE_NAME, AXUM_SESSION_USER_ID_KEY};
 use async_session::SessionStore;
 use async_sqlx_session::PostgresSessionStore;
 use axum::extract::{FromRequest, RequestParts, TypedHeader};
 use axum::headers::Cookie;
-use axum::response::Redirect;
+use axum::response::Json;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+use crate::constants::{database_url, AXUM_SESSION_COOKIE_NAME, AXUM_SESSION_USER_ID_KEY};
 
 #[derive(Deserialize, Serialize)]
 pub struct UserContext {
@@ -16,10 +18,10 @@ impl<B> FromRequest<B> for UserContext
 where
     B: Send,
 {
-    type Rejection = Redirect;
+    type Rejection = Json<serde_json::Value>;
 
-    async  fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        let redirect = || Redirect::to("/login");
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let redirect = || Json(json!({"status": "ng", "errorMessage": "session expired"}));
 
         let database_url = database_url();
         let store = PostgresSessionStore::new(&database_url)
@@ -33,7 +35,7 @@ where
         let session = store
             .load_session(session_str.to_string())
             .await
-            .map_err(|_|  redirect())?;
+            .map_err(|_| redirect())?;
         let session = session.ok_or(redirect())?;
         let context = UserContext {
             user_id: session.get::<i32>(AXUM_SESSION_USER_ID_KEY).unwrap(),
