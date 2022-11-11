7835d184bb48d1e526dc7615cc027cd71deb2626
Alingof <Alignof@outlook.com>
2022-11-11 23:21:35 +0900

    [fix] fix argument type

--- web_server/src/controllers/accounts.rs
+++ web_server/src/controllers/accounts.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::Extension,
+    extract::{self, Extension},
     response::{AppendHeaders, IntoResponse},
     http::header::SET_COOKIE,
     Json,
@@ -11,23 +11,23 @@ use crate::database::RepositoryProvider;
 use crate::services;
 
 pub async fn register(
-    form: Json<SignUp>,
+    extract::Json(account_data): extract::Json<SignUp>,
     Extension(repository_provider): Extension<RepositoryProvider>
 ) -> impl IntoResponse {
     tracing::debug!("/api/register");
     let account_repo = repository_provider.accounts();
     services::create_account(
         &account_repo,
-        &form.name,
-        &form.password,
+        &account_data.name,
+        &account_data.password,
     )
     .await;
-    let (id, session_token) = services::create_session(&account_repo, &form.name, &form.password).await;
+    let (id, session_token) = services::create_session(&account_repo, &account_data.name, &account_data.password).await;
 
     match session_token {
         Some(session_token) => {
             let headers = AppendHeaders([(SET_COOKIE, session_token.cookie())]);
-            let response = Json(AccountResponse::new(id.unwrap(), form.name.clone()));
+            let response = Json(AccountResponse::new(id.unwrap(), account_data.name.clone()));
             Ok((headers, response))
         },
         None => Err(Json(AccountResponse::error()))
@@ -35,17 +35,17 @@ pub async fn register(
 }
 
 pub async fn login(
-    form: Json<SignIn>,
+    extract::Json(account_data): extract::Json<SignIn>,
     Extension(repository_provider): Extension<RepositoryProvider>,
 ) -> Result<impl IntoResponse, impl IntoResponse> {
     tracing::debug!("/api/login");
     let account_repo = repository_provider.accounts();
-    let (id, session_token) = services::create_session(&account_repo, &form.name, &form.password).await;
+    let (id, session_token) = services::create_session(&account_repo, &account_data.name, &account_data.password).await;
 
     match session_token {
         Some(session_token) => {
             let headers = AppendHeaders([(SET_COOKIE, session_token.cookie())]);
-            let response = Json(AccountResponse::new(id.unwrap(), form.name.clone()));
+            let response = Json(AccountResponse::new(id.unwrap(), account_data.name.clone()));
             Ok((headers, response))
         },
         None => Err(Json(AccountResponse::error()))
