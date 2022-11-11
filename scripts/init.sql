afa960ee2cb70e8356141c731c2d102b9f7a479e
Alingof <Alignof@outlook.com>
2022-11-11 21:26:42 +0900

    [wip][update] update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,21 +1,22 @@
-CREATE TYPE JudgeResult AS ENUM (
-    'Pending',
-    'AC',
-    'WA',
-    'AE',
-    'LE',
-    'TLE',
-    'SystemError'
-);
-
-CREATE TABLE accounts ( -- ユーザ
+CREATE TABLE accounts -- ユーザ
+(
 	id serial primary key,
 	name text unique not null,
-	password text not null
-    -- session data?
+	password text not null,
+	score integer not null
 );
 
-CREATE TABLE probrems ( -- 問題
+CREATE TABLE sessions
+(
+	session_key text primary key,
+	user_id integer not null,
+	created_at timestamptz not null,
+	FOREIGN KEY (user_id) REFERENCES accounts(id)
+		ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE probrems -- 問題
+(
 	id serial primary key,
 	title text not null,
 	statement text not null,
@@ -25,15 +26,20 @@ CREATE TABLE probrems ( -- 問題
 	score integer not null
 );
 
-CREATE TABLE submits ( -- submit
+CREATE TABLE submits -- submit
+(
 	id serial primary key,
-	user_id serial not null,
+	user_id integer not null,
+	problem_id integer not null,
 	time timestamptz not null,
 	asem text not null,
-	result JudgeResult not null
+	result serial not null
+	-- FOREIGN KEY (user_id, problem_id) REFERENCES accounts(id), problems(id)
+	-- 	ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
-CREATE TABLE testcases ( -- テストケース
+CREATE TABLE testcases -- テストケース
+(
 	id serial primary key,
 	sepecial_cond serial,
 	input text not null,
