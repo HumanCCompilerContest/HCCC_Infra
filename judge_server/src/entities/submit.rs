e781eac70043fe46ea8317aab2418570d3daecc2
Alingof <Alignof@outlook.com>
2022-11-14 19:12:55 +0900

    [add] add problem_id to Submit

--- judge_server/src/entities/submit.rs
+++ judge_server/src/entities/submit.rs
@@ -4,28 +4,32 @@ use postgres_types::{ToSql, FromSql};
 #[derive(Debug, ToSql, FromSql)]
 #[postgres(name = "judgeresult")]
 pub enum JudgeResult {
-    Pending,
     AC,
     WA,
     AE,
     LE,
+    RE,
     TLE,
+    Pending,
     SystemError,
 }
 
+#[derive(Debug, ToSql, FromSql)]
 pub struct Submit {
     id: i32,
     pub user_id: i32,
+    pub problem_id: i32,
     pub time: DateTime<Utc>,
     pub asm: String,
     pub result: JudgeResult,
 }
 
 impl Submit {
-    pub fn new(id: i32, user_id: i32, time: DateTime<Utc>, asm: String, result: JudgeResult) -> Submit {
+    pub fn new(id: i32, user_id: i32, problem_id: i32, time: DateTime<Utc>, asm: String, result: JudgeResult) -> Self {
         Submit {
             id,
             user_id,
+            problem_id,
             time,
             asm,
             result,
