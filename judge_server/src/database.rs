4411c6921c2831439eba249f0c003e3149d91735
Alingof <Alignof@outlook.com>
2022-11-14 19:13:17 +0900

    [add] add new_repo

--- judge_server/src/database.rs
+++ judge_server/src/database.rs
@@ -1,20 +1,17 @@
-use axum::Extension;
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
-use std::env;
 use tokio_postgres::NoTls;
 
 use crate::repos_impl::SubmitImpl;
 
 pub type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 
-pub async fn layer() -> Extension<RepositoryProvider> {
-    dotenv::dotenv().ok();
-    let database_url = env::var("DATABASE_URL").unwrap();
+pub async fn new_repo() -> RepositoryProvider {
+    let database_url = std::env::var("DATABASE_URL").unwrap();
     let manager = PostgresConnectionManager::new_from_stringlike(database_url, NoTls).unwrap();
     let pool = Pool::builder().build(manager).await.unwrap();
 
-    Extension(RepositoryProvider(pool))
+    RepositoryProvider(pool)
 }
 
 #[derive(Clone)]
