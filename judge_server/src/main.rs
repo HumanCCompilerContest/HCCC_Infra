0a02a16639b75358f8d447f9adce38c039e8b0be
Alingof <Alignof@outlook.com>
2022-11-15 12:05:02 +0900

    [add] add a function judge

--- judge_server/src/main.rs
+++ judge_server/src/main.rs
@@ -1,8 +1,37 @@
 use judge_server::database::new_repo;
-use judge_server::entities::JudgeResult;
+use judge_server::entities::{Submit, JudgeResult};
 use judge_server::repositories::submit::Submits;
 use tokio::process::Command;
 use tokio::time::{sleep, Duration};
+
+async fn judge(repo_submit: &dyn Submits, submit: Submit) {
+    let result = Command::new("bash")
+        .arg("-c")
+        .arg(dbg!(format!(
+            "sudo docker run --memory=128M --cpus=\"0.05\" ghcr.io/alignof/hccc_infra:judge_system-develop {}",
+            base64::encode(&submit.asm)
+        )))
+        .output()
+        .await;
+
+    if let Ok(result) = result {
+        let judge_result = match result.status.code().unwrap_or(6) {
+            0 => JudgeResult::AC,
+            1 => JudgeResult::WA,
+            2 => JudgeResult::AE,
+            3 => JudgeResult::LE,
+            4 => JudgeResult::RE,
+            5 => JudgeResult::TLE,
+            _ => JudgeResult::SystemError,
+        };
+
+        repo_submit.store_result(judge_result, submit.id()).await;
+    } else {
+        repo_submit
+            .store_result(JudgeResult::SystemError, submit.id())
+            .await;
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -14,39 +43,11 @@ async fn main() {
     let repo = new_repo().await;
     let repo_submit = repo.submit();
     loop {
-        let submit = match repo_submit.get_pending_submit().await {
-            Some(s) => s,
-            None => {
-                continue;
-            }
-        };
-
-        let result = Command::new("bash")
-            .arg("-c")
-            .arg(dbg!(format!(
-                "sudo docker run --memory=128M --cpus=\"0.05\" ghcr.io/alignof/hccc_infra:judge_system-develop {}",
-                base64::encode(&submit.asm)
-            )))
-            .output()
-            .await;
-
-        if let Ok(result) = result {
-            let judge_result = match result.status.code().unwrap_or(6) {
-                0 => JudgeResult::AC,
-                1 => JudgeResult::WA,
-                2 => JudgeResult::AE,
-                3 => JudgeResult::LE,
-                4 => JudgeResult::RE,
-                5 => JudgeResult::TLE,
-                _ => JudgeResult::SystemError,
-            };
-
-            repo_submit.store_result(judge_result, submit.id()).await;
-        } else {
-            repo_submit
-                .store_result(JudgeResult::SystemError, submit.id())
-                .await;
+        let submits = repo_submit.get_pending_submits().await;
+        for submit in submits {
+            judge(&repo_submit, submit).await;
         }
+
         sleep(Duration::from_millis(5000)).await;
     }
 }
