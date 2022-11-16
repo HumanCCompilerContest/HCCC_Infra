74c937107ad88cf3632d611ecf868d5ee965f430
Alingof <Alignof@outlook.com>
2022-11-16 23:16:28 +0900

    [fix] fix to create ranking

--- web_server/src/repos_impl/users.rs
+++ web_server/src/repos_impl/users.rs
@@ -31,7 +31,7 @@ impl<'a> Users for UserImpl<'a> {
         let conn = self.pool.get().await.unwrap();
         let ac = conn
             .query(
-                "SELECT a_outer.name AS name, coalesce(SUM(sub.score),0) AS score, MAX(sub.min_time) AS max_time
+                "SELECT a_outer.name AS name, coalesce(SUM(sub.score), 0) AS score, coalesce(MAX(sub.min_time), NOW()) AS max_time
                 FROM
                 (
                     SELECT a.name AS name, s.problem_id AS problem_id, p.score AS score, MIN(s.time) AS min_time FROM submits AS s 
@@ -48,16 +48,16 @@ impl<'a> Users for UserImpl<'a> {
             )
             .await
             .unwrap();
+
         let not_ac = conn
             .query(
-                "SELECT a.name AS name, COUNT(*) AS wrong_count FROM submits AS s
+                "SELECT a.name AS name, COUNT( (s.result != 'AC' AND s.result != 'WC') or NULL) AS wrong_count FROM submits AS s
                 JOIN (
                 SELECT user_id, problem_id FROM submits
                     WHERE result = 'AC'
                     GROUP BY user_id, problem_id
                 ) AS sub ON s.user_id = sub.user_id AND s.problem_id = sub.problem_id
-                JOIN accounts AS a ON s.user_id = a.id
-                WHERE s.result != 'AC' AND s.result != 'WC'
+                RIGHT JOIN accounts AS a ON s.user_id = a.id
                 GROUP BY a.name
                 ORDER BY a.name
                 ;",
@@ -67,21 +67,25 @@ impl<'a> Users for UserImpl<'a> {
             .unwrap();
 
         // return null when wc is null
-        let mut ranking = dbg!(ac)
+        let mut ranking = ac
             .iter()
-            .map(|x| {
-                let name = x.get("name");
-                let nac = not_ac
-                    .iter()
-                    .find(|y| y.get::<&str, String>("name") == name)
-                    .map(|y| y.get("wrong_count"))
-                    .unwrap_or(0);
-
-                Rank::new(name, std::cmp::max(0, x.get::<&str, i64>("score") - nac))
+            .zip(not_ac.iter())
+            .map(|(x, y)| {
+                Rank::new(
+                    x.get("name"),
+                    std::cmp::max(
+                        0,
+                        x.get::<&str, i64>("score") - y.get::<&str, i64>("wrong_count"),
+                    ),
+                    x.get("max_time"),
+                )
             })
             .collect::<Vec<Rank>>();
 
-        ranking.sort_by(|x, y| (-x.score).cmp(&-y.score));
+        ranking.sort_by(|x, y| match (-x.score).cmp(&-y.score) {
+            std::cmp::Ordering::Equal => x.time.cmp(&y.time),
+            other => other,
+        });
         dbg!(ranking)
             .into_iter()
             .enumerate()
