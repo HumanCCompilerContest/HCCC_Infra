688e3e8fc23b0fbd154d8429984988b74395a270
Alingof <Alignof@outlook.com>
2023-09-23 14:06:12 +0900

    [fix] modify to get error_line_number from problems table

--- judge_server/src/repos_impl/problem.rs
+++ judge_server/src/repos_impl/problem.rs
@@ -14,12 +14,15 @@ impl<'a> Problems for ProblemImpl<'a> {
     /// Get pending submits from database.
     async fn get_all_problems(&self) -> Vec<Problem> {
         let conn = self.pool.get().await.unwrap();
-        conn.query("SELECT id, test_target, is_wrong_code FROM problems", &[])
-            .await
-            .unwrap()
-            .into_iter()
-            .map(std::convert::Into::into)
-            .collect()
+        conn.query(
+            "SELECT id, test_target, is_wrong_code, error_line_number FROM problems",
+            &[],
+        )
+        .await
+        .unwrap()
+        .into_iter()
+        .map(std::convert::Into::into)
+        .collect()
     }
 }
 
